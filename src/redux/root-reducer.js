907dd6d14c9a993b63e5b30b4008719d3186ca0b
ghevondtunyan <60566859+ghevondtunyan@users.noreply.github.com>
2020-11-26 14:31:05 +0400

    moved our directory and shop data, created respectiv selectors and updated corresponding components with new redux flow for directory/shop

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,28 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import userReduser from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import durectoryRedcer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReduser,
   cart: cartReducer,
+  directory: durectoryRedcer,
+  shop: shopReducer,
 });
+
+// export default combineReducers({
+//   user: userReduser,
+//   cart: cartReducer,
+// });
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import { persistStore } from "redux-persist";
 import rootReduser from "./root-reducer";
 
 const middleWare = [logger];
 
-const store = createStore(rootReduser, applyMiddleware(...middleWare));
+export const store = createStore(rootReduser, applyMiddleware(...middleWare));
 
-export default store;
+// export default store;
+
+export const persistor = persistStore(store);
+
+export default { store, persistor };
